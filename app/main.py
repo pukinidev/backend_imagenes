5f773b5db18cdd80f52751a73c7f264055adb77f
pukinidev <v.caro@uniandes.edu.co>
2025-05-23 16:47:29 -0500

    fixed cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app.core.database import create_db_and_tables
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import api
 
 @asynccontextmanager
@@ -10,10 +11,12 @@ async def lifespan(app: FastAPI):
     
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api)
-    
-    
-
-    
-    
-
